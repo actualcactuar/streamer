158717d4e4f8a2a8d4aba1c19485644bc342813e
jiri <kiviniemi.jiri@gmail.com>
2021-02-05 10:17:54 +0200

    load more content to stream when playable content ends

--- public/view.js
+++ public/view.js
@@ -15,7 +15,7 @@ videoSource.addEventListener('sourceopen', async () => {
     const response = await fetch(`/stream/${streamName}/index.txt`);
     if(!response.ok) return;
     const files = (await response.text()).split('\n');
-    console.log(files)
+    let lastRead;
 
     for (const file of files) {
         const url = `/stream/${streamName}/${file}`;
@@ -23,9 +23,34 @@ videoSource.addEventListener('sourceopen', async () => {
         const buffer = await response.arrayBuffer();
         sourceBuffer.appendBuffer(buffer);
         await once('onupdateend', sourceBuffer);
+        lastRead = file;
     }
 
-    videoSource.endOfStream();
+    video.currentTime = video.buffered.end(0) - 4;
+
+    video.onwaiting = async () => {
+        const response = await fetch(`/stream/${streamName}/index.txt`);
+        if(!response.ok) return;
+        const f = (await response.text()).split('\n');
+        const newFiles = f.slice(f.indexOf(lastRead) + 1)
+
+        for (const file of newFiles) {
+            const url = `/stream/${streamName}/${file}`;
+            const response = await fetch(url, { headers: { 'Content-Type': 'application/octet-stream' } });
+            const buffer = await response.arrayBuffer();
+            sourceBuffer.appendBuffer(buffer);
+            await once('onupdateend', sourceBuffer);
+            lastRead = file;
+        }
+
+        if(!newFiles.length){
+            videoSource.endOfStream();
+        }
+
+    }
+
 
 })
+
+
 video.src = videoUrl;
